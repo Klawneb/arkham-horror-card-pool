5508ab0419e6f96eea8df14abc7ae651f5eb276d
Klawneb <benjaminwalker222@gmail.com>
2024-05-15 21:19:32 +0100

    add ability to cycle through cards in modal

--- src/Components/CardDisplay.tsx
+++ src/Components/CardDisplay.tsx
@@ -2,19 +2,25 @@
 import {Image, Modal, ModalBody, ModalContent, Pagination, Spinner, useDisclosure} from "@nextui-org/react";
 import {Card} from "../types.ts";
 import {useFilterStore, usePageStore} from "../stores.ts";
-import {RefObject, useEffect, useRef, useState} from "react";
+import {Dispatch, RefObject, SetStateAction, useEffect, useRef, useState} from "react";
 import {useAutoAnimate} from "@formkit/auto-animate/react";
 import parse from "html-react-parser"
 import {filterCards} from "../cardFilter.ts";
+import useKeyDown from "../hooks/useKeyDown.ts";
 
 export default function CardDisplay() {
     const cards = useQuery({queryKey: ["cards"], queryFn: getCards});
     const filterStore = useFilterStore();
     const cardDisplayRef = useRef<HTMLDivElement>(null);
     const [cardsVisible, setCardsVisible] = useState(0)
+    const [modalCard, setModalCard] = useState<Card | undefined>()
     const pageStore = usePageStore()
     const filteredCards = filterCards(filterStore.filter, cards.data ? cards.data : [], cardsVisible)
     const [animationParent] = useAutoAnimate()
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    useKeyDown(showNextCard, ["ArrowRight"]);
+    useKeyDown(showPrevCard, ["ArrowLeft"]);
+    
 
     useEffect(() => {
         function updateCardsVisible(cardDisplay: RefObject<HTMLDivElement>) {
@@ -42,6 +48,24 @@ export default function CardDisplay() {
         return await cards.json();
     }
 
+    function showNextCard() {
+        if (!modalCard) {
+            return;
+        }
+
+        const cardIndex = filteredCards.findIndex((card) => card === modalCard);
+        setModalCard(filteredCards[Math.min(cardIndex + 1, filteredCards.length)]);
+    }
+
+    function showPrevCard() {
+        if (!modalCard) {
+            return;
+        }
+
+        const cardIndex = filteredCards.findIndex((card) => card === modalCard);
+        setModalCard(filteredCards[Math.max(cardIndex - 1, 0)]);
+    }
+
     if (cards.data === undefined) {
         return <div ref={cardDisplayRef} className="flex flex-grow items-center justify-center">
             <Spinner label="Loading cards..." size="lg"/>
@@ -57,7 +81,8 @@ export default function CardDisplay() {
                     filteredCards
                         .slice(cardsVisible * (pageStore.page - 1), cardsVisible * pageStore.page)
                         .map((card) => {
-                            return <CardInstance key={card.octgn_id} card={card}/>
+                            return <CardInstance key={card.octgn_id} card={card} onOpen={onOpen}
+                                                 setModalCard={setModalCard}/>
                         })
                 }
             </div>
@@ -67,47 +92,87 @@ export default function CardDisplay() {
             <Pagination total={Math.floor(filteredCards.length / cardsVisible)} className="pf-0" page={pageStore.page}
                         onChange={pageStore.setPage}/>
         </div>
+        <CardModal card={modalCard} isOpen={isOpen} onOpenChange={onOpenChange} showNextCard={showNextCard} showPrevCard={showPrevCard}/>
     </div>
 }
 
-function CardInstance({card}: { card: Card }) {
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+interface CardInstanceProps {
+    card: Card,
+    onOpen: () => void,
+    setModalCard: Dispatch<SetStateAction<Card | undefined>>
+}
 
+function CardInstance({card, onOpen, setModalCard}: CardInstanceProps) {
     return <div className="h-[280px] flex flex-col items-center justify-center">
-        <Image src={"https://arkhamdb.com/" + card.imagesrc} height={280} width={202} onClick={onOpen}
+        <Image src={"https://arkhamdb.com/" + card.imagesrc} height={280} width={202} onClick={() => {
+            setModalCard(card)
+            onOpen();
+        }}
                className="hover:cursor-pointer"/>
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange} hideCloseButton={true} size="5xl" backdrop={"blur"}
-               className="border-0 dark">
-            <ModalContent className="bg-zinc-800">
-                <ModalBody className="p-0">
-                    <div className="flex">
+    </div>
+}
+
+interface CardModalProps {
+    card: Card | undefined,
+    isOpen: boolean,
+    onOpenChange: () => void,
+    showNextCard: () => void,
+    showPrevCard: () => void,
+}
+
+function CardModal({card, isOpen, onOpenChange, showNextCard, showPrevCard}: CardModalProps) {
+    if (!card) {
+        return null
+    }
+
+    return <Modal isOpen={isOpen} onOpenChange={onOpenChange} hideCloseButton={true} size="5xl" backdrop={"blur"}
+                  className="border-0 dark">
+
+        <ModalContent className="bg-zinc-800 overflow-visible">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2}
+                 stroke="currentColor"
+                 className="w-20 h-20 absolute -left-24 top-1/2 transform -translate-y-1/2 stroke-foreground transition-all hover:cursor-pointer hover:h-28 hover:w-28 hover:-left-28"
+                onClick={showPrevCard}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"/>
+            </svg>
+
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2}
+                 className="w-20 h-20 absolute -right-24 top-1/2 transform -translate-y-1/2 stroke-foreground transition-all hover:cursor-pointer hover:h-28 hover:w-28 hover:-right-28"
+                 onClick={showNextCard}
+            >
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
+            </svg>
+            <ModalBody className="p-0">
+                <div className="flex">
+                    <div className="min-w-[432px] min-h-[600px]">
                         <Image src={"https://arkhamdb.com/" + card.imagesrc} height={600} width={432}
-                               className="min-w-[432px] min-h-[600px]"/>
-                        <div className="p-10 flex flex-col">
-                            <p className="text-3xl text-foreground font-bold mb-4">{card.name}</p>
-                            <p className="text-xl text-foreground">
-                                <span className="font-bold tracking-wide whitespace-pre">Cost: </span>{card.cost ?? 0}
-                            </p>
-                            <p className="text-xl text-foreground">
-                                <span className="font-bold tracking-wide whitespace-pre">XP: </span>{card.xp ?? 0}</p>
-                            <p className="text-xl text-foreground">
+                               className="min-w-[432px] min-h-[600px] select-none"/>
+                    </div>
+                    <div className="p-10 flex flex-col">
+                        <p className="text-3xl text-foreground font-bold mb-4">{card.name}</p>
+                        <p className="text-xl text-foreground">
+                            <span className="font-bold tracking-wide whitespace-pre">Cost: </span>{card.cost ?? 0}
+                        </p>
+                        <p className="text-xl text-foreground">
+                            <span className="font-bold tracking-wide whitespace-pre">XP: </span>{card.xp ?? 0}</p>
+                        <p className="text-xl text-foreground">
                                 <span
                                     className="font-bold tracking-wide whitespace-pre">Faction: </span>{card.faction_name}
-                            </p>
-                            <div className="flex-grow flex flex-col items-center justify-center">
-                                <p className="text-xl text-foreground">{parse(card.real_text ?? "")}</p>
-                                {
-                                    card.back_text ?
-                                        <p className="text-xl text-foreground mt-6">{parse(card.back_text ?? "")}</p>
-                                        :
-                                        null
-                                }
-                            </div>
-                            <p className="text-xl text-foreground font-bold">{card.pack_name}</p>
+                        </p>
+                        <div className="flex-grow flex flex-col items-center justify-center">
+                            <p className="text-xl text-foreground">{parse(card.real_text ?? "")}</p>
+                            {
+                                card.back_text ?
+                                    <p className="text-xl text-foreground mt-6">{parse(card.back_text ?? "")}</p>
+                                    :
+                                    null
+                            }
                         </div>
+                        <p className="text-xl text-foreground font-bold">{card.pack_name}</p>
                     </div>
-                </ModalBody>
-            </ModalContent>
-        </Modal>
-    </div>
+                </div>
+            </ModalBody>
+        </ModalContent>
+    </Modal>
 }
---
+++ src/hooks/useKeyDown.ts
@@ -0,0 +1,20 @@
+﻿import {useEffect} from "react";
+
+export default function useKeyDown(callback: () => void, keys: string[]) {
+    useEffect(() => {
+        function onKeyDown(e: KeyboardEvent) {
+            const wasKeyPressed = keys.some((key) => e.key === key);
+
+            if (wasKeyPressed) {
+                e.preventDefault();
+                callback();
+            }
+        }
+        
+        document.addEventListener("keydown", onKeyDown);
+        
+        return () => {
+            document.removeEventListener("keydown", onKeyDown);
+        }
+    })
+}
